9a9f0a0298994a29277a54666fa51602fdcf5d28
minusexp10 <akshat.kumar05j@gmail.com>
2026-02-22 00:48:45 +0530

    JWT configured

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,30 @@
-const express = require("express")
-const app = express()
-const session = require("express-session")
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const cors = require("cors");
 
-app.use(express.json())
+// CORS SHOULD ALWAYS COME BEFORE ROUTES
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+app.use(express.json());
 
 app.use(session({
-    secret:"Shashwat_randi",
-    resave: false,
-    saveUninitialized: false
-}))
+  secret: "Shashwat_randi",
+  resave: false,
+  saveUninitialized: false
+}));
 
-const authRoutes = require('./routes/authRoutes')
-const appRoutes = require('./routes/appRoutes')
+const authRoutes = require('./routes/authRoutes');
+const appRoutes = require('./routes/appRoutes');
 
-app.use('/auth' , authRoutes)
-app.use('/app', appRoutes)
+app.use('/auth', authRoutes);
+app.use('/app', appRoutes);
 
-app.get('/' , (req,res) => {
-    res.send("Server is running..");
-})
+app.get('/', (req, res) => {
+  res.send("Server is running..");
+});
 
 module.exports = app;
